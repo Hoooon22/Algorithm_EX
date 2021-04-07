13705e44a470f9cee91ef16a98d9bd0d67c1e27e
Hoooon22 <momo990305@gmail.com>
2021-04-07 17:23:50 +0900

    create 2021_04 route make 07__StringBasic

---
+++ 2021_04/07_StringBasic/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07_StringBasic
+{
+    class Solution
+    {
+        public bool solution(string s)
+        {
+            bool answer = true;
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
