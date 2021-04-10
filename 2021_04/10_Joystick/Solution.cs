5e6cb915a70e04ff566f9a5a8413280826e3dc40
Hoooon22 <momo990305@gmail.com>
2021-04-10 16:33:08 +0900

    update

--- 2021_04/10_Joystick/Solution.cs
+++ 2021_04/10_Joystick/Solution.cs
@@ -1,9 +1,21 @@
-﻿using System;
+﻿/*
+ 알고리즘 문제 : 프로그래머스 코딩테스트 연습 - 조이스틱
+ 실습 날짜 : 2021-04-10
+ 문제 : https://programmers.co.kr/learn/courses/30/lessons/42860
+ */
+
+using System;
 
 namespace _10_Joystick
 {
     class Solution
     {
+        static public int solution(string name)
+        {
+            int answer = 0;
+            return answer;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
