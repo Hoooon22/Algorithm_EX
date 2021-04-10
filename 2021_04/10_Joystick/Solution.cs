7ccf6f15c27a0420b6090b6f913996f5ee411e10
Hoooon22 <momo990305@gmail.com>
2021-04-10 16:08:53 +0900

    make 10_Joystick cs

---
+++ 2021_04/10_Joystick/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10_Joystick
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
