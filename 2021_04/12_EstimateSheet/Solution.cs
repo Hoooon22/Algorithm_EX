a0dcace78470ae4dc819223f1b48d4a75c0faff3
Hoooon22 <momo990305@gmail.com>
2021-04-12 19:21:53 +0900

    solution

---
+++ 2021_04/12_EstimateSheet/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _12_EstimateSheet
+{
+    class Solution
+    {
+        static public int solution(int n, int a, int b)
+        {
+            int answer = 3;
+
+            for (int i = 0; i < n; i++)
+            {
+
+            }
+
+            // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+            System.Console.WriteLine("Hello C#");
+
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
