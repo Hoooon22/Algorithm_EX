dd2a2ae10bbe29dfecbb4d734d964a9af501253a
Hoooon22 <momo990305@gmail.com>
2021-04-14 16:07:30 +0900

    make _14_Immigration cs

---
+++ 2021_04/14_Immigration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _14_Immigration
+{
+    class Program
+    {
+        static public long solution(int n, int[] times)
+        {
+            long answer = 0;
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
