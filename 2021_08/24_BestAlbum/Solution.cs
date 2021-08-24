96b6b2852fd198717aa71616087f9f2f7ef8df62
Hoooon22 <momo990305@gmail.com>
2021-08-24 21:49:35 +0900

    rename Solution.cs

---
+++ 2021_08/24_BestAlbum/Solution.cs
@@ -0,0 +1,23 @@
+﻿/*
+ 알고리즘 문제 : 프로그래머스 코딩테스트 연습 - 베스트앨범
+ 실습 날짜 : 2021-08-24
+ 문제 : https://programmers.co.kr/learn/courses/30/lessons/42579?language=csharp
+ */
+using System;
+
+namespace _24_BestAlbum
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public int[] solution(string[] genres, int[] plays)
+        {
+            int[] answer = new int[] { };
+            return answer;
+        }
+    }
+}
