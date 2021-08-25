7a397f51b1979e6f10a1cf4f0302202beeaa8bb6
Hoooon22 <momo990305@gmail.com>
2021-08-25 21:57:54 +0900

    sync

--- 2021_08/24_BestAlbum/Solution.cs
+++ 2021_08/24_BestAlbum/Solution.cs
@@ -1,6 +1,6 @@
 ﻿/*
  알고리즘 문제 : 프로그래머스 코딩테스트 연습 - 베스트앨범
- 실습 날짜 : 2021-08-24
+ 실습 날짜 : 2021-08-24 
  문제 : https://programmers.co.kr/learn/courses/30/lessons/42579?language=csharp
  */
 using System;
