753823397e08bd91256900e1afc8243d5d7addd8
Hoooon22 <momo990305@gmail.com>
2021-10-25 22:27:49 +0900

    snyc

--- 2021_10/25_TargetNumber/Solution.cs
+++ 2021_10/25_TargetNumber/Solution.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ 알고리즘 문제 : 프로그래머스 코딩테스트 연습 - 타겟 넘버
+ 실습 날짜 : 2021-10-25
+ 문제 : https://programmers.co.kr/learn/courses/30/lessons/43165?language=csharp
+ */
+
+using System;
 
 namespace _25_TargetNumber
 {
