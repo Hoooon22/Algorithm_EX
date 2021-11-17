6318aeb56f23fc2fcaae2612338ed4ce64eaf2b6
Hoooon22 <momo990305@gmail.com>
2021-11-17 21:18:12 +0900

    Create 17_BTree

---
+++ 2021_11/17_BTree/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _17_BTree
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
