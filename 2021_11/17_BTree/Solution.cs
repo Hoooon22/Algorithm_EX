ea27a4161f3e682b82825de6325f9dd2944cf727
Hoooon22 <momo990305@gmail.com>
2021-11-30 20:22:55 +0900

    create Node class

--- 2021_11/17_BTree/Solution.cs
+++ 2021_11/17_BTree/Solution.cs
@@ -2,6 +2,14 @@
 
 namespace _17_BTree
 {
+    class Node
+    {
+        public int data;
+        public Node left;
+        public Node mid;
+        public Node right;
+    }
+
     class Solution
     {
         static void Main(string[] args)
