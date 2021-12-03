9a45e26df6320a188d7fa847bf7a2e52ea25caf0
Hoooon22 <momo990305@gmail.com>
2021-12-03 19:11:21 +0900

    start AppendNode case 1

--- 2021_11/17_BTree/MainClass.cs
+++ 2021_11/17_BTree/MainClass.cs
@@ -34,7 +34,13 @@ namespace _17_BTree
             }
             else
             {
+                Node tail = head;
 
+                // case 1. if there is no division
+                if (newNode.data > tail.data)
+                {
+
+                }
             }
         }
     }
