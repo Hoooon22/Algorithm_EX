f5a02ded783753b5e9694a061637fd01c72dc38e
momo990305@gmail.com <momo990305@gmail.com>
2022-02-19 15:12:24 +0900

    2022_02_19_CheckDistance

---
+++ 2022_02/19_CheckDistance/19_CheckDistance/Solution.cs
@@ -0,0 +1,23 @@
+﻿/*
+ 알고리즘 문제 : 거리두기 확인하기 - 2021 카카오 채용연계형 인턴십
+ 실습 날짜 : 2022-02-19
+ 문제 : https://programmers.co.kr/learn/courses/30/lessons/81302?language=csharp
+ */
+using System;
+
+namespace _CheckDistance
+{
+    class Solution
+    {
+        public int[] solution(string[,] places)
+        {
+            int[] answer = new int[] { };
+            return answer;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
