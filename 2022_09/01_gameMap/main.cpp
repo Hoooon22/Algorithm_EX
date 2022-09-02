6f4fa882634350acae333c547f60c320d1fab0a0
momo990305@gmail.com <momo990305@gmail.com>
2022-09-02 14:01:14 +0900

    bfs

--- 2022_09/01_gameMap/main.cpp
+++ 2022_09/01_gameMap/main.cpp
@@ -5,30 +5,86 @@
  */
 
 #include <iostream>
+#include <queue>
 #include <algorithm>
 #include <vector>
 
 using namespace std;
 
-int bfs(vector<vector<int>> maps, bool visited[], int count)
+int bfs(vector<vector<int> > maps, bool visited[][100])
 {
+    queue<pair<int, int>> q;
+    q.push(make_pair(0,0));
+    vector<vector<int>> dist;
+    dist[0][0] = 1;
 
+    // until queue is empty
+    while (!q.empty())
+    {
+        pair<int,int> x = q.front();
+        q.pop();
 
-    return count;
+        // add no visited element in 4 way
+        // up
+        if (x.first - 1 >= 0)
+        {
+            if (!visited[x.first-1][x.second])
+            {
+                q.push(make_pair(x.first-1, x.second));
+                visited[x.first-1][x.second] = true;
+                dist[x.first-1][x.second] = dist[x.first][x.second] + 1;
+            }
+        }
+        // down
+        if (x.first + 1 < maps.size())
+        {
+            if (!visited[x.first+1][x.second])
+            {
+            q.push(make_pair(x.first+1, x.second));
+            visited[x.first+1][x.second] = true;
+            dist[x.first+1][x.second] = dist[x.first][x.second] + 1;
+            }
+        }
+        // left
+        if (x.second - 1 >= 0)
+        {
+            if (!visited[x.first][x.second-1])
+            {
+            q.push(make_pair(x.first, x.second-1));
+            visited[x.first][x.second-1] = true;
+            dist[x.first][x.second-1] = dist[x.first][x.second] + 1;
+            }
+        }
+        // right
+        if (x.second + 1 < maps[0].size())
+        {
+            if (!visited[x.first][x.second+1])
+            {
+                q.push(make_pair(x.first, x.second+1));
+                visited[x.first][x.second+1] = true;
+                dist[x.first][x.second+1] = dist[x.first][x.second] + 1;
+            }   
+        }
+    }
+
+    return dist[maps[0].size()-1][maps.size()-1];
 }
 
-int solution(vector<vector<int>> maps)
+int solution(vector<vector<int> > maps)
 {
     int answer = 0;
-    bool visited[10000]; // max size : 100x100
+    bool visited[100][100]; // max size : 100x100
+    for (int i = 0; i < 100; i++)
+    {
+        for (int j = 0; j < 100; j++)
+        {
+            visited[i][j]= false;
+        }
+    }
     
-    
+    visited[0][0] = true; // first node
+
+    answer = bfs(maps, visited);
 
     return answer;
-}
-
-int main()
-{
-
-    return 0;
 }
