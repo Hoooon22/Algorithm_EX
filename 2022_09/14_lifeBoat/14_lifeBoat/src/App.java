6b577a587da02fc339ed650a8efb7609f935bb5d
momo990305@gmail.com <momo990305@gmail.com>
2022-09-16 11:42:01 +0900

    first

--- 2022_09/14_lifeBoat/14_lifeBoat/src/App.java
+++ 2022_09/14_lifeBoat/14_lifeBoat/src/App.java
@@ -2,18 +2,34 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
-
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        Boolean isfull = false;
+        Vector<Integer> V_boat = new Vector<Integer>();
+        Vector<Integer> V_people = new Vector<Integer>();
+        for (int i : people) {
+            V_people.add(i);
+        }
+        V_people.sort(c);
 
+        V_boat.add(people[0]);
+        for (int i = 1; i < people.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (V_boat.get(j) + i <= limit) {
+                    int tmp = V_boat.get(j);
+                    V_boat.remove(j);
+                    V_boat.add(j, tmp+i);
+                    isfull = true;
+                    break;
+                }
+            }
+            if (isfull) {
+                V_boat.add(i);
+            }
+        }
 
-
+        answer = V_boat.size();
         return answer;
     }
 }
