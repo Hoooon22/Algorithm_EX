f4f59104b0b0f2a8049f7de6771f9e5e0f138a22
momo990305@gmail.com <momo990305@gmail.com>
2022-09-19 15:52:51 +0900

    use minHeap

--- 2022_09/19_dualPriorityQueue/Soulution/src/Solution.java
+++ 2022_09/19_dualPriorityQueue/Soulution/src/Solution.java
@@ -1,5 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+
+    public int[] solution(String[] operations) {
+        int[] answer = {};
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+
+        return answer;
     }
 }
