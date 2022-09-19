707e60bc4e9f25ac9d8503730768b3a4287c0589
momo990305@gmail.com <momo990305@gmail.com>
2022-09-19 16:53:49 +0900

    why array over?

--- 2022_09/19_dualPriorityQueue/Soulution/src/Solution.java
+++ 2022_09/19_dualPriorityQueue/Soulution/src/Solution.java
@@ -1,6 +1,4 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
@@ -10,19 +8,27 @@ public class Solution {
     public int[] solution(String[] operations) {
         int[] answer = {};
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
         // for operations
         for (int i = 0; i < operations.length; i++) {
             if (operations[i].charAt(0)== 'I') {
                 minHeap.add(Integer.parseInt(operations[i].substring(2)));
+                maxHeap.add(Integer.parseInt(operations[i].substring(2)));
             }
-            else if (operations[i] == "D 1") {
-
-            }
-            else if (operations[i] == "D -1") {
-                
+            else if (!minHeap.isEmpty()) {
+                if (operations[i] == "D 1") {
+                    minHeap.remove();
+                    maxHeap.remove(Integer.parseInt(operations[i].substring(operations[i].length()-1)));
+                }
+                else if (operations[i] == "D -1") {
+                    maxHeap.remove();
+                    minHeap.remove(Integer.parseInt(operations[i].substring(operations[i].length()-1)));
+                }
             }
         }
+        answer[0] = minHeap.poll();
+        answer[1] = maxHeap.poll();
 
         return answer;
     }
