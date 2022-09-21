657b67dea4af8fb79202e9657e43ec8234992075
momo990305@gmail.com <momo990305@gmail.com>
2022-09-21 13:15:35 +0900

    create 2022/09/22 IntegerTriangle

---
+++ 2022_09/21_integerTriangle/src/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public int solution(int[][] triangle) {
+        int answer = 0;
+
+        
+
+        return answer;
+    }
+}
