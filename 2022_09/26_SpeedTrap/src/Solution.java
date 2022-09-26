3bcd175c620b5c8648ab6e912766835a8c3d5e84
momo990305@gmail.com <momo990305@gmail.com>
2022-09-26 15:25:54 +0900

    add 2022/09/26 SpeedTrap

---
+++ 2022_09/26_SpeedTrap/src/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+    
+    public int solution(int[][] routes) {
+        int answer = 0;
+        return answer;
+    }
+}
