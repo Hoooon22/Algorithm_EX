30c422b664f3ab97d5d5f0b8d0f1a3c101cacb54
momo990305@gmail.com <momo990305@gmail.com>
2022-09-26 22:06:20 +0900

    done!

--- 2022_09/26_SpeedTrap/src/Solution.java
+++ 2022_09/26_SpeedTrap/src/Solution.java
@@ -1,12 +1,21 @@
-public class Solution {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+import java.util.Arrays;
+import java.util.Comparator;
+import java.io.*;
 
-    public int solution(int[][] routes) {
+public class Solution {
+
+    public static int solution(int[][] routes) {
         int answer = 0;
         int[][] camera = new int[routes.length][routes[0].length];
         boolean isOverlap = false;
+
+        // sort
+        Arrays.sort(routes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                    return o1[1] - o2[1]; 
+            }
+        });
 
         // init
         camera[0][0] = routes[0][0];
@@ -16,7 +25,17 @@ public class Solution {
         for (int i = 1; i < routes.length; i++) {
             for (int j = 0; j < answer; j++) {
                 // ! 겹치는 상황이 하나가 아님 !
-                if ((routes[i][1] >= camera[j][0] && routes[i][0] <= camera[j][1]) || (routes[i][0] <= camera[j][1] && routes[i][1] >= camera[j][0])) { //  범위 안에 있다면
+                if (routes[i][0] <= camera[j][0] && routes[i][1] >= camera[j][1]) {
+                    isOverlap = true;
+                    break;
+                }
+                else if (routes[i][1] <= camera[j][0] && routes[i][0] >= camera[j][1]) { // 왼쪽 범위 안에 있다면
+                    camera[j][1] = routes[i][1];
+                    isOverlap = true;
+                    break;
+                }
+                else if (routes[i][1] >= camera[j][0] && routes[i][0] <= camera[j][1]) { // 오른쪽 범위 안에 있다면
+                    camera[j][0] = routes[i][0];
                     isOverlap = true;
                     break;
                 }
@@ -30,5 +49,9 @@ public class Solution {
         }
 
         return answer;
+    }
+    public static void main(String[] args) throws Exception {
+        int[][] array = {{2, 2},{0, 1},{-10,9}};
+        System.out.println(solution(array));
     }
 }
