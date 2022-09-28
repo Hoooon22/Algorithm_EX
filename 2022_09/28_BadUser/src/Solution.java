bb8bb1ad6e4ec196abe8c49469885cd9915db158
momo990305@gmail.com <momo990305@gmail.com>
2022-09-28 16:29:32 +0900

    create 2022/09/28 Solution.java

---
+++ 2022_09/28_BadUser/src/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+    public int solution(String[] user_id, String[] banned_id) {
+        int answer = 0;
+        return answer;
+    }
+}
