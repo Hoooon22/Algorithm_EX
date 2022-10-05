e2550c48b42ab5b99e55c242fd23ba16be0af59b
momo990305@gmail.com <momo990305@gmail.com>
2022-10-05 15:26:08 +0900

    create 2022/10/05 pickupitem

---
+++ 2022_10/05_PickUpItem/src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
