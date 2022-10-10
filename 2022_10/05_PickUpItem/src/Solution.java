aa037927cf35b187790b1e7b4dc6ad755bd39ff3
momo990305@gmail.com <momo990305@gmail.com>
2022-10-11 01:33:33 +0900

    ver.01

--- 2022_10/05_PickUpItem/src/Solution.java
+++ 2022_10/05_PickUpItem/src/Solution.java
@@ -1,10 +1,120 @@
 public class Solution {
 
+    public boolean isGoal(int currentX, int currentY, int itemX, int itemY) {
+        if (currentX == itemX && currentY == itemY) {
+            return true;
+        }
+        return false;
+    }
+
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
+        int[][] dir_clock = new int[50][50];
+        int[][] dir_counterclock = new int[50][50];
+        int dir_clock_count = 0;
+        int dir_counterclock_count = 0;
+        int currentX = characterX;
+        int currentY = characterY;
 
-        
+        // 2 way in -> first way = clockwise
+        // 1 - down / 2 - right / 3 - up / 4 - left /
+        // box's direction setting
+        for (int i = 0; i < rectangle.length; i++) {
+            // left side - up
+            for (int j = rectangle[i][1]; j <= rectangle[i][3]; j++) {
+                if (dir_clock[rectangle[i][0]][j] < 3) {
+                    dir_clock[rectangle[i][0]][j] = 3;
+                }
+            }
+            // top side - right
+            for (int j = rectangle[i][0]; j <= rectangle[i][2]; j++) {
+                if (dir_clock[j][rectangle[i][3]] < 2) {
+                    dir_clock[j][rectangle[i][3]] = 2;
+                }
+            }
+            // right side - down
+            for (int j = rectangle[i][1]; j < rectangle[i][3]; j++) {
+                if (dir_clock[rectangle[i][2]][j] == 4) {
+                    dir_clock[rectangle[i][2]][j] = 1;
+                }
+            }
+            // bottom side - left
+            for (int j = rectangle[i][0]; j <= rectangle[i][2]; j++) {
+                if (dir_clock[j][rectangle[i][1]] < 4) {
+                    dir_clock[j][rectangle[i][1]] = 4;
+                }
+            }
+        }
+        while (!isGoal(currentX, currentY, itemX, itemY)) {
+            switch (dir_clock[currentX][currentY]) {
+                case 1:
+                    currentX--;
+                    break;
+                case 2:
+                    currentY++;
+                    break;
+                case 3:
+                    currentX++;
+                    break;
+                case 4:
+                    currentY--;
+                    break;
+                default:
+                    break;
+            }
+            dir_clock_count++;
+        }
 
+        // 2 way in -> second way = counterclockwise
+        //  1 - down / 2 - left / 3 - up / 4 - right /
+        // box's direction setting
+        for (int i = 0; i < rectangle.length; i++) {
+            // left side - down
+            for (int j = rectangle[i][1]; j <= rectangle[i][3]; j++) {
+                if (dir_counterclock[rectangle[i][0]][j] == 4) {
+                    dir_counterclock[rectangle[i][0]][j] = 1;
+                }
+            }
+            // top side - left
+            for (int j = rectangle[i][0]; j <= rectangle[i][2]; j++) {
+                if (dir_counterclock[j][rectangle[i][3]] < 2) {
+                    dir_counterclock[j][rectangle[i][3]] = 2;
+                }
+            }
+            // right side - up
+            for (int j = rectangle[i][1]; j < rectangle[i][3]; j++) {
+                if (dir_counterclock[rectangle[i][2]][j] < 3) {
+                    dir_counterclock[rectangle[i][2]][j] = 3;
+                }
+            }
+            // bottom side - right
+            for (int j = rectangle[i][0]; j <= rectangle[i][2]; j++) {
+                if (dir_counterclock[j][rectangle[i][1]] < 4) {
+                    dir_counterclock[j][rectangle[i][1]] = 4;
+                }
+            }
+        }
+        while (!isGoal(currentX, currentY, itemX, itemY)) {
+            switch (dir_counterclock[currentX][currentY]) {
+                case 1:
+                    currentX--;
+                    break;
+                case 2:
+                    currentY++;
+                    break;
+                case 3:
+                    currentX++;
+                    break;
+                case 4:
+                    currentY--;
+                    break;
+                default:
+                    break;
+            }
+            dir_counterclock_count++;
+        }
+
+        answer = (dir_clock_count > dir_counterclock_count) ? dir_clock_count : dir_counterclock_count;
         return answer;
     }
 
