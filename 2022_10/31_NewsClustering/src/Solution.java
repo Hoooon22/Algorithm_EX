9a7ceb49181e3e7ca9c15735dd399bea39a47a51
momo990305@gmail.com <momo990305@gmail.com>
2022-10-31 22:41:26 +0900

    create 31_NewsClustering

---
+++ 2022_10/31_NewsClustering/src/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public int solution(String str1, String str2) {
+        int answer = 0;
+
+         
+
+        return answer;
+    }
+}
