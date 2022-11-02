55b49c8fb4aa0fc7ed4b57175099aaa3ba91a887
momo990305@gmail.com <momo990305@gmail.com>
2022-11-02 14:50:40 +0900

    Upload 20220211 triproute

---
+++ 2022_11/02_TripRoute/src/Solution.java
@@ -0,0 +1,19 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 여행 경로
+ 실습 날짜 : 2022-11-02
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/43164
+*/
+
+public class Solution {
+
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
