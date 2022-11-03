5e506d755b37a2a7b7476920f2f1b531d95972fd
momo990305@gmail.com <momo990305@gmail.com>
2022-11-03 14:35:57 +0900

    create 2022/11/03 GoToSchool

---
+++ 2022_11/03_GoToSchool/src/Solution.java
@@ -0,0 +1,19 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 등굣길
+ 실습 날짜 : 2022-11-03
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/42898
+*/
+import java.util.*;
+
+public class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
