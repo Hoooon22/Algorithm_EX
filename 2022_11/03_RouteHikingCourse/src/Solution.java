fd0097f074783ac8d2d1f7b2f2886c18dc85d63e
momo990305@gmail.com <momo990305@gmail.com>
2022-11-03 21:12:14 +0900

    Create 2022/11/03 RouteHikingCourse

---
+++ 2022_11/03_RouteHikingCourse/src/Solution.java
@@ -0,0 +1,17 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 등산코스 정하기
+ 실습 날짜 : 2022-11-03
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/118669
+*/
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
