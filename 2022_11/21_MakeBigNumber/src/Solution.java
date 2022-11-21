dbd8f0dff479e3cf6e0fbad5a16d25993f8483e7
momo990305@gmail.com <momo990305@gmail.com>
2022-11-21 18:20:28 +0900

    create 22/11/21 MakeBigNumber

---
+++ 2022_11/21_MakeBigNumber/src/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public String solution(String number, int k) {
+        String answer = "";
+
+        
+
+        return answer;
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
