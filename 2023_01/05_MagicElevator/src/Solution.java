9a09927fe5f31ab7a640ea8fc4532ffac7dbade9
momo990305@gmail.com <momo990305@gmail.com>
2023-01-06 00:22:14 +0900

    upload 20230105

---
+++ 2023_01/05_MagicElevator/src/Solution.java
@@ -0,0 +1,19 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 마법의 엘리베이터
+ 실습 날짜 : 2023-01-05
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/148653?language=java
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public int solution(int storey) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+}
