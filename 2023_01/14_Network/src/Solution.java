aab3bd29bc561bcb2237133390391c3083010312
momo990305@gmail.com <momo990305@gmail.com>
2023-01-26 14:49:27 +0900

    0126 upload

--- 2023_01/14_Network/src/Solution.java
+++ 2023_01/14_Network/src/Solution.java
@@ -1,5 +1,33 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 네트워크
+ 실습 날짜 : 2023-01-14
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/43162
+*/
+import java.io.*;
+import java.util.*;
+
+class Graph {
+    private int V; // 노드의 개수
+    private LinkedList<Integer> adj[][]; // 인접 리스트
+
+    // 생성자
+    Graph(int v) {
+        V = v;
+        adj = new LinkedList[v][v];
+        
+    }
+}
+
 public class Solution {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+
+    public static int solution(int n, int[][] computers) {
+        int answer = 0;
+
+
+
+        return answer;
     }
 }
