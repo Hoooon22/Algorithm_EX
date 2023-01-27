7a442033450bb37454ace805367cec7c62db75c9
momo990305@gmail.com <momo990305@gmail.com>
2023-01-27 12:55:49 +0900

    add 230127_N

--- 2023_01/27_N/src/Solution.java
+++ 2023_01/27_N/src/Solution.java
@@ -2,4 +2,12 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public static int solution(int N, int number) {
+        int answer = 0;
+
+        
+
+        return answer;
+    }
 }
