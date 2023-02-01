71115e4b7fdb41e58fbdb47971f0db52a72e3443
momo990305@gmail.com <momo990305@gmail.com>
2023-02-01 19:15:22 +0900

    upload 20230201

---
+++ 2023_02/01_Camouflage/src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        
+        return answer;
+    }
+}
