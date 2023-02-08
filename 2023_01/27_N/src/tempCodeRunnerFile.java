b945a55c5d574c529d6ea57478294e7bd605a354
momo990305@gmail.com <momo990305@gmail.com>
2023-02-08 20:26:48 +0900

    solution

--- 2023_01/27_N/src/tempCodeRunnerFile.java
+++
@@ -1,5 +0,0 @@
-public static int calculation(int N, int number) {
-
-    // }
-
-    // public static int using_N()
--- 2023_02/01_Camouflage/src/Solution.java
+++ 2023_02/01_Camouflage/src/Solution.java
@@ -1,3 +1,10 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - N으로 표현
+ 실습 날짜 : 2023-02-01
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/42578
+*/
+import java.util.*;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
@@ -5,7 +12,19 @@ public class Solution {
 
     public int solution(String[][] clothes) {
         int answer = 0;
+        String kinds;
+
+        // make hash
+        String[][] clothes_sort = new String[clothes.length][2];
+        for (int i = 0; i < clothes.length; i++) {
+            clothes_sort[i][0] = clothes[i][1];
+            clothes_sort[i][1] = clothes[i][0];
+        }
+        Arrays.sort(clothes_sort);
+
+        // 1개, 2개, 3개, ... N개
         
+
         return answer;
     }
 }
