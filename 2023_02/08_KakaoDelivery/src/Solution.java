445c5ea65e42c2f966b176932506d577bd0a8f79
momo990305@gmail.com <momo990305@gmail.com>
2023-02-08 21:02:44 +0900

    create 08_KakaoDelivery

---
+++ 2023_02/08_KakaoDelivery/src/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = -1;
+
+        
+
+        return answer;
+    }
+}
