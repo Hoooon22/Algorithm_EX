8bf41baec9af11ceae67b2e3a27503be619052d7
momo990305@gmail.com <momo990305@gmail.com>
2023-03-17 20:45:23 +0900

    2023/03/17

---
+++ 2023_03/17_PaintOver/src/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+
+        
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+}
