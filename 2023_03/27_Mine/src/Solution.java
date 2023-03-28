ee44a01a36058822affe5ef3cb9decee8731c621
momo990305@gmail.com <momo990305@gmail.com>
2023-03-28 18:40:34 +0900

    solution

--- 2023_03/27_Mine/src/Solution.java
+++ 2023_03/27_Mine/src/Solution.java
@@ -2,4 +2,9 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public int solution(int[] picks, String[] minerals) {
+        int answer = 0;
+        return answer;
+    }
 }
