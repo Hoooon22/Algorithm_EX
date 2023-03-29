54cc98a22dc4f7c9cb4436ba945b2f1e7c15f654
momo990305@gmail.com <momo990305@gmail.com>
2023-03-29 15:15:05 +0900

    create 230329_RicochetRobot

---
+++ 2023_03/29_RicochetRobot/src/Solution.java
@@ -0,0 +1,19 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 무인도 여행
+ 실습 날짜 : 2023-03-16
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/150368
+*/
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public int solution(String[] board) {
+        int answer = 0;
+        
+        return answer;
+    }
+}
