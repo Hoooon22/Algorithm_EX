ec56bb0024f0aeb19e0a6cdfbd6f78dfbe8c3f0d
momo990305@gmail.com <momo990305@gmail.com>
2023-03-31 23:50:54 +0900

    remain calculation of goal

--- 2023_03/29_RicochetRobot/src/Solution.java
+++ 2023_03/29_RicochetRobot/src/Solution.java
@@ -39,12 +39,13 @@ public class Solution {
             arr_board[arr_board.length-1][i] = 'D';
         }
 
-        answer = bfs(arr_board, start[0], start[1], 0);
+        answer = bfs(arr_board, start[0], start[1]);
 
         return answer;
     }
 
-    public static int bfs(char[][] arr_board, int x, int y, int goal) {
+    public static int bfs(char[][] arr_board, int x, int y) {
+        int goal = 0;
         boolean visited[][] = new boolean[arr_board.length][arr_board[0].length];
         LinkedList<int[]> queue = new LinkedList<int[]>();
         int[] dy = {0,0,1,-1};
@@ -59,7 +60,8 @@ public class Solution {
             var curr = queue.remove();
             var curr_x = curr[0];
             var curr_y = curr[1];
-            goal++;
+
+            // printMap(arr_board, curr_x, curr_y);
 
             // 4방향 벽까지 상하좌우 이동 
             for (int i = 0; i < dx.length; i++) {
@@ -67,33 +69,29 @@ public class Solution {
                 var nx = curr_x;
                 var ny = curr_y;
 
-                System.out.print(nx + " ");
-                System.out.println(ny);
+                // 다음 방향이 바로 벽이 아니라면, 벽에 부딪힐때 까지 전진
+                if (arr_board[nx+dx[i]][ny+dy[i]] != 'D') {
+                    for (int j = 1; j < 100; j++) {
+                        // 한칸 전진
+                        nx += dx[i];
+                        ny += dy[i];
 
-                // 다음 방향이 바로 벽이라면 넘어감
-                if (arr_board[nx+dx[i]][ny+dy[i]] == 'D') 
-                    continue;
-                // 아니라면, 벽에 부딪힐때 까지 전진
-                for (int j = 1; j < 100; j++) {
-                    // 한칸 전진
-                    nx += dx[i];
-                    ny += dy[i];
-
-                    if (arr_board[nx+dx[i]][ny+dy[i]] == 'D' && arr_board[nx][ny] == 'G') { // 현재 기준, 다음 블럭이 벽이고 지금 블럭이 목표지점이라면
-                        // 리턴
-                        return goal;                        
+                        if (arr_board[nx+dx[i]][ny+dy[i]] == 'D' && arr_board[nx][ny] == 'G') { // 현재 기준, 다음 블럭이 벽이고 지금 블럭이 목표지점이라면
+                            // 리턴
+                            return goal;                        
+                        }
+                        else if (arr_board[nx+dx[i]][ny+dy[i]] == 'D' && !visited[nx][ny]) { // 현재 기준, 다음 블럭이 벽이면서 방문 안했다면
+                            // 현재 위치를 큐에 저장
+                            visited[nx][ny] = true;
+                            queue.add(new int[]{nx, ny});
+                            break;
+                        }
+                        else if (arr_board[nx+dx[i]][ny+dy[i]] == 'D' && visited[nx][ny]) { // 벽인데 방문했었다면,
+                            // 스킵
+                            break;
+                        }
+                        // 나머지는 다음으로 전진
                     }
-                    else if (arr_board[nx+dx[i]][ny+dy[i]] == 'D' && !visited[nx][ny]) { // 현재 기준, 다음 블럭이 벽이면서 방문했다면
-                        // 현재 위치를 큐에 저장
-                        visited[nx][ny] = true;
-                        queue.add(new int[]{nx, ny});
-                        break;
-                    }
-                    else if (arr_board[nx+dx[i]][ny+dy[i]] == 'D' && visited[nx][ny]) { // 벽인데 만났다면,
-                        // 스킵
-                        break;
-                    }
-                    // 나머지는 다음으로 전진
                 }
             }
         }
@@ -101,4 +99,19 @@ public class Solution {
         // 찾지 못했다면, -1
         return -1;
     }
+
+    // public static void printMap(char[][] arr_board, int x, int y) {
+    //     for (int i = 0; i < arr_board.length; i++) {
+    //         for (int j = 0; j < arr_board[0].length; j++) {
+    //             if (i == x && j == y) {
+    //                 System.out.print('R');
+    //             }
+    //             else {
+    //                 System.out.print(arr_board[i][j]);
+    //             }
+    //         }
+    //         System.out.println();
+    //     }
+    //     System.out.println();
+    // }
 }
