b1bbcef0ab3ddf173af268bcab8c81a9b864595e
momo990305@gmail.com <momo990305@gmail.com>
2023-04-05 23:22:29 +0900

    create 230405_Purse

---
+++ 2023_04/05_purse/src/Solution.java
@@ -0,0 +1,21 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 연속 펄스 부분 수열의 합
+ 실습 날짜 : 2023-04-05
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/161988
+*/
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public long solution(int[] sequence) {
+        long answer = 0;
+
+        
+
+        return answer;
+    }
+}
