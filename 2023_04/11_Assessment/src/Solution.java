e3527f0aef98917f7ac25e28d8e7b5092f5054b9
momo990305@gmail.com <momo990305@gmail.com>
2023-04-11 15:38:40 +0900

    create 230411_Assessment

---
+++ 2023_04/11_Assessment/src/Solution.java
@@ -0,0 +1,18 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 인사고과
+ 실습 날짜 : 2023-04-11
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/152995
+*/
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public int solution(int[][] scores) {
+        int answer = 0;
+        return answer;
+    }
+}
