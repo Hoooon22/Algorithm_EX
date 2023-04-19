e1ee4f88a6807eb6507c99122fafb96f5d19a2d5
momo990305@gmail.com <momo990305@gmail.com>
2023-04-19 16:20:27 +0900

    create 230419_InterceptSystem

---
+++ 2023_04/19_InterceptSystem/src/Solution.java
@@ -0,0 +1,18 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 요격 시스템
+ 실습 날짜 : 2023-04-19
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/181188
+*/
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public int solution(int[][] targets) {
+        int answer = 0;
+        return answer;
+    }
+}
