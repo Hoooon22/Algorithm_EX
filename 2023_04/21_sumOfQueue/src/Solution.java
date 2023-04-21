679742c236085f0d11a0e5c8402ad398e0cf6a3c
momo990305@gmail.com <momo990305@gmail.com>
2023-04-21 17:36:21 +0900

    96.7%

--- 2023_04/21_sumOfQueue/src/Solution.java
+++ 2023_04/21_sumOfQueue/src/Solution.java
@@ -8,7 +8,44 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        int answer = -2;
+        int answer = 0;
+        Queue<Integer> q1 = new LinkedList<Integer>();
+        Queue<Integer> q2 = new LinkedList<Integer>();
+        long sum = 0; // q1 - q2
+        
+        // sum
+        for (int i = 0; i < queue1.length; i++) {
+            sum += queue1[i] - queue2[i];
+        }
+        
+        
+        // queue to q
+        for (int i = 0; i < queue1.length; i++) {
+            q1.add(queue1[i]);
+            q2.add(queue2[i]);
+        }
+        
+        // 
+        while (sum != 0) {
+            if (answer > queue1.length + queue2.length) {
+                return -1;
+            }
+            else {
+                if (sum > 0) { // q1 > q2
+                    int tmp = q1.poll();
+                    sum -= tmp*2;
+                    q2.add(tmp);
+                }
+                else { // q1 < q2
+                    int tmp = q2.poll();
+                    sum += tmp*2;
+                    q1.add(tmp);
+                }
+                answer++;    
+            }
+        }
+        
+        
         return answer;
     }
 }
