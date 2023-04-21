ae547d136c685833afb8ac73b39b349338b70a05
momo990305@gmail.com <momo990305@gmail.com>
2023-04-21 16:28:27 +0900

    create 230421_sumOfQueue

---
+++ 2023_04/21_sumOfQueue/src/Solution.java
@@ -0,0 +1,14 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 두 큐 합 같게 만들기
+ 실습 날짜 : 2023-04-21
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/118667
+*/
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = -2;
+        return answer;
+    }
+}
