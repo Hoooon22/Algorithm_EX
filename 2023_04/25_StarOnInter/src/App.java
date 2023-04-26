3ad94b8e7ebf2b6e99af0e5b190042303401a6cb
momo990305@gmail.com <momo990305@gmail.com>
2023-04-26 23:53:49 +0900

    82.8

---
+++ 2023_04/25_StarOnInter/src/App.java
@@ -0,0 +1,104 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 교점에 별 만들기
+ 실습 날짜 : 2023-04-25
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/87377
+*/
+
+import java.util.*;
+
+class Solution {
+    public String[] solution(int[][] line) {
+        List<int[]> list = new LinkedList<>();
+        
+        // 교점 찾기
+        for (int i = 0; i < line.length-1; i++) {
+            for (int j = i+1; j < line.length; j++) {
+                if (isInter(line[i], line[j])) {
+                    double[] test = intersection(line[i], line[j]);
+                    if (test[0] % 1 == 0.0 && test[1] % 1 == 0.0) {
+                        int[] inter = {(int)test[0], (int)test[1]};
+                        list.add(inter);
+                        // System.out.printf("%d, %d\n", inter[0], inter[1]);
+                    }
+                }
+            }
+        }
+        
+        // list to arr
+        int listSize = list.size();
+        int[][] resultArr = list.toArray(new int[listSize][2]);
+        
+        // 맵 크기 설정 및 생성
+        int[] min = {100000, 100000};
+        int[] max = {-100000, -100000};
+        for(int i = 0; i < resultArr.length; i++) {
+            // min max x,y
+            if (resultArr[i][0] < min[0]) {
+                min[0] = resultArr[i][0];
+            }
+            if (resultArr[i][1] < min[1]) {
+                min[1] = resultArr[i][1];
+            }
+            if (resultArr[i][0] > max[0]) {
+                max[0] = resultArr[i][0];
+            }
+            if (resultArr[i][1] > max[1]) {
+                max[1] = resultArr[i][1];
+            }
+        }
+        // System.out.printf("%d, %d, %d, %d\n", min[0], min[1], max[0], max[1]);
+
+        // 맵 구현
+        String[] answer = new String[Math.abs(max[1] - min[1])+1];
+        for (int i = 0; i < answer.length; i++) {
+            String str = ".".repeat(Math.abs(max[0] - min[0])+1);
+            answer[i] = str;
+            // System.out.println(str);
+        }
+        // 별 표시
+        for (int i = 0; i < resultArr.length; i++) {
+            String str = "";
+            int x = resultArr[i][0] - min[0];
+            int y = Math.abs(max[1] - min[1]) - (resultArr[i][1] - min[1]);
+            // System.out.printf("%d, %d\n", x,y);
+            
+            for (int j = 0; j < answer[y].length(); j++) {
+                if (j == x) {
+                    str += "*";
+                }
+                else {
+                    str += answer[y].charAt(j);
+                }
+            }
+            
+            answer[y] = str;
+            // System.out.println(answer[y]);
+        }
+        
+        // 출력
+        for (int i = 0; i < answer.length; i++) {
+            // System.out.println(answer[i]);
+        }
+
+        return answer;
+    }
+    
+    public static boolean isInter(int[] line1, int[] line2) {
+        // AD - BC = 0인 경우 두 직선은 평행 또는 일치
+        if ((line1[0] * line2[1] - line1[1] * line2[0]) == 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    
+    public static double[] intersection(int[] line1, int[] line2) {
+        double[] result = new double[2];
+        
+        result[0] = (double)(line1[1] * line2[2] - line1[2] * line2[1]) / (double)(line1[0] * line2[1] - line1[1] * line2[0]);
+        result[1] = (double)(line1[2] * line2[0] - line1[0] * line2[2]) / (double)(line1[0] * line2[1] - line1[1] * line2[0]);
+        
+        return result;
+    }
+}
