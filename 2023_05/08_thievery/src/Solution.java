046048dedf219342219336e8331431ca8a46d127
momo990305@gmail.com <momo990305@gmail.com>
2023-05-08 15:53:35 +0900

    create 230508_Thievery

---
+++ 2023_05/08_thievery/src/Solution.java
@@ -0,0 +1,14 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 도둑질
+ 실습 날짜 : 2023-05-08
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/42897
+*/
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] money) {
+        int answer = 0;
+        return answer;
+    }
+}
