618ee8441b08100044d85c67296ec855089eef16
momo990305@gmail.com <momo990305@gmail.com>
2023-05-24 19:11:51 +0900

    create 230524_Billiard

---
+++ 2023_05/24_Billiard/src/Solution.java
@@ -0,0 +1,17 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 당구 연습
+ 실습 날짜 : 2023-05-24
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/169198
+*/
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = {};
+        
+        
+        
+        return answer;
+    }
+}
