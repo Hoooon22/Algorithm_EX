bb81de0ba3a045e1b6ff1d259c7781138f304e72
momo990305@gmail.com <momo990305@gmail.com>
2023-05-26 14:07:11 +0900

    create 230526_MazeCommand

---
+++ 2023_05/26_MazeCommand/src/Solution.java
@@ -0,0 +1,12 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 미로 탈출 명령어
+ 실습 날짜 : 2023-05-26
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/150365
+*/
+
+class Solution {
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        String answer = "";
+        return answer;
+    }
+}
