cb01545a46b824286064abe99ea2ba31334e0520
momo990305@gmail.com <momo990305@gmail.com>
2023-05-31 19:14:02 +0900

    create 230531_FillThePuzzle

---
+++ 2023_05/31_FillThePuzzle/src/Solution.java
@@ -0,0 +1,12 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 미로 탈출 명령어
+ 실습 날짜 : 2023-05-31
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/84021
+*/
+
+class Solution {
+    public int solution(int[][] game_board, int[][] table) {
+        int answer = -1;
+        return answer;
+    }
+}
