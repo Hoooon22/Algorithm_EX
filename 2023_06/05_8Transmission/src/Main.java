9cda388a6b82ad39fb4c1dcde0b93fef0fb40c8b
momo990305@gmail.com <momo990305@gmail.com>
2023-06-05 21:38:19 +0900

    create 230605_8Transmission

---
+++ 2023_06/05_8Transmission/src/Main.java
@@ -0,0 +1,17 @@
+/*
+ 알고리즘 문제 : Softeer - 8단 변속기
+ 실습 날짜 : 2023-06-05
+ 문제 : https://softeer.ai/practice/info.do?idx=1&eid=408
+*/
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        
+    }
+}
