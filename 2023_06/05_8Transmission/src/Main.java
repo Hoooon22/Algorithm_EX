7290b3d493ea9959dd7296899bb27ac8b1c299f5
momo990305@gmail.com <momo990305@gmail.com>
2023-06-05 21:56:31 +0900

    100%

--- 2023_06/05_8Transmission/src/Main.java
+++ 2023_06/05_8Transmission/src/Main.java
@@ -12,6 +12,47 @@ public class Main
 {
     public static void main(String args[])
     {
-        
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[8];
+
+        arr[0] = sc.nextInt();
+        arr[1] = sc.nextInt();
+        arr[2] = sc.nextInt();
+        arr[3] = sc.nextInt();
+        arr[4] = sc.nextInt();
+        arr[5] = sc.nextInt();
+        arr[6] = sc.nextInt();
+        arr[7] = sc.nextInt();
+
+        if (arr[0] == 1) {
+            int tmp = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] != tmp + 1) {
+                    System.out.println("mixed");
+                    return;
+                }
+                tmp++;
+            }
+
+            System.out.println("ascending");
+            return;
+        }
+        else if (arr[0] == 8) {
+            int tmp = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] != tmp - 1) {
+                    System.out.println("mixed");
+                    return;
+                }
+                tmp--;
+            }
+
+            System.out.println("descending");
+            return;
+        }
+        else {
+            System.out.println("mixed");
+            return;
+        }
     }
 }
