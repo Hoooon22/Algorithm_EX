c93e2823ba48683e0372db96d47c661074bdf1dd
momo990305@gmail.com <momo990305@gmail.com>
2023-06-07 16:34:51 +0900

    create 230607_EvalGrade

---
+++ 2023_06/07_EvalGrade/src/Main.java
@@ -0,0 +1,17 @@
+/*
+ 알고리즘 문제 : Softeer - 성적평가
+ 실습 날짜 : 2023-06-07
+ 문제 : https://softeer.ai/practice/formCodeEditor.do
+*/
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        
+    }
+}
