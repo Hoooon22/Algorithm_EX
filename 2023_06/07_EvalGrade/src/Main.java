df157450d633084800538631a8b6bc4b4a946ce4
momo990305@gmail.com <momo990305@gmail.com>
2023-06-09 21:57:36 +0900

    time out

--- 2023_06/07_EvalGrade/src/Main.java
+++ 2023_06/07_EvalGrade/src/Main.java
@@ -12,6 +12,62 @@ public class Main
 {
     public static void main(String args[])
     {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int scores[][] = new int[3][n];
+
+        // init scores
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < n; j++) {
+                scores[i][j] = sc.nextInt();
+            }
+        }
+
+        // rating
+        for (int i = 0; i < 3; i++) {
+            Integer sort_arr[] = new Integer[n];
+            for (int j = 0; j < n; j++) {
+                sort_arr[j] = scores[i][j];
+            }
+            Arrays.sort(sort_arr, Collections.reverseOrder());
+
+            for (int j = 0; j < n; j++) {
+                int rate = 1;
+                for (int num : sort_arr) {
+                    if (scores[i][j] == num) {
+                        System.out.printf("%d ", rate);
+                        break;
+                    }
+                    else {
+                        rate++;
+                    }
+                }
+            }
+            System.out.println();
+        }
         
+        // last sum
+        int sum[] = new int[n];
+        Integer sort_sum[] = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            sum[i] = scores[0][i] + scores[1][i] + scores[2][i];
+            sort_sum[i] = sum[i];
+        }
+
+        Arrays.sort(sort_sum, Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            int rate = 1;
+            for (int num : sort_sum) {
+                if (sum[i] == num) {
+                    System.out.printf("%d ", rate);
+                    break;
+                }
+                else {
+                    rate++;
+                }
+            }
+        }
     }
 }
