2881825e6d748314c251ebb210ed0bbcef0ac597
momo990305@gmail.com <momo990305@gmail.com>
2023-06-09 22:48:25 +0900

    70%

--- 2023_06/07_EvalGrade/src/Main.java
+++ 2023_06/07_EvalGrade/src/Main.java
@@ -7,67 +7,44 @@
 import java.util.*;
 import java.io.*;
 
-
-public class Main
-{
-    public static void main(String args[])
-    {
+public class Main {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
         int scores[][] = new int[3][n];
+        int sum[] = new int[n];
 
-        // init scores
+        // init scores and calculate sum
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < n; j++) {
                 scores[i][j] = sc.nextInt();
+                sum[j] += scores[i][j];
             }
         }
 
         // rating
         for (int i = 0; i < 3; i++) {
-            Integer sort_arr[] = new Integer[n];
-            for (int j = 0; j < n; j++) {
-                sort_arr[j] = scores[i][j];
-            }
-            Arrays.sort(sort_arr, Collections.reverseOrder());
-
             for (int j = 0; j < n; j++) {
                 int rate = 1;
-                for (int num : sort_arr) {
-                    if (scores[i][j] == num) {
-                        System.out.printf("%d ", rate);
-                        break;
-                    }
-                    else {
+                for (int k = 0; k < n; k++) {
+                    if (j != k && scores[i][j] < scores[i][k])
                         rate++;
-                    }
                 }
+                System.out.printf("%d ", rate);
             }
             System.out.println();
         }
-        
-        // last sum
-        int sum[] = new int[n];
-        Integer sort_sum[] = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            sum[i] = scores[0][i] + scores[1][i] + scores[2][i];
-            sort_sum[i] = sum[i];
-        }
 
-        Arrays.sort(sort_sum, Collections.reverseOrder());
+        // last sum rating
         for (int i = 0; i < n; i++) {
             int rate = 1;
-            for (int num : sort_sum) {
-                if (sum[i] == num) {
-                    System.out.printf("%d ", rate);
-                    break;
-                }
-                else {
+            for (int j = 0; j < n; j++) {
+                if (i != j && sum[i] < sum[j])
                     rate++;
-                }
             }
+            System.out.printf("%d ", rate);
         }
     }
 }
