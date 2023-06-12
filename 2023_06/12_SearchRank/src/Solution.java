5276ceebc3d5928dd21e287a466b3a7760dff37e
momo990305@gmail.com <momo990305@gmail.com>
2023-06-12 18:30:03 +0900

    create 230612_SearchRank

---
+++ 2023_06/12_SearchRank/src/Solution.java
@@ -0,0 +1,14 @@
+/*
+ 알고리즘 문제 : 프로그래머스 - 순위 검색
+ 실습 날짜 : 2023-06-12
+ 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/72412
+*/
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] info, String[] query) {
+        int[] answer = {};
+        return answer;
+    }
+}
