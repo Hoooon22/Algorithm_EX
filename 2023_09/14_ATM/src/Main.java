f8053351e549c58b8f15c8bc92d19d4f5ea13667
momo990305@gmail.com <momo990305@gmail.com>
2023-09-15 03:03:33 +0900

    clear 20230914_ATM

--- 2023_09/14_ATM/src/Main.java
+++ 2023_09/14_ATM/src/Main.java
@@ -1,8 +1,35 @@
+
+/*
+ * Title : ATM
+ * Link : https://www.acmicpc.net/problem/11399
+ */
 import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        
+        int size;
+        int arr[];
+        int answer = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        size = Integer.parseInt(st.nextToken());
+        arr = new int[size];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < size; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // sort
+        Arrays.sort(arr);
+
+        // sum
+        for (int i = 0; i < size; i++) {
+            answer += arr[i] * (size - i);
+        }
+
+        System.out.println(answer);
     }
 }
