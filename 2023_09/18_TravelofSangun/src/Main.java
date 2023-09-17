f90105bca18b8a98a0cd6cfe8d7dad61b5c90770
momo990305@gmail.com <momo990305@gmail.com>
2023-09-17 14:50:13 +0900

    input

--- 2023_09/18_TravelofSangun/src/Main.java
+++ 2023_09/18_TravelofSangun/src/Main.java
@@ -1,8 +1,46 @@
+/*
+ * Title : 상근이의 여행
+ * Link : https://www.acmicpc.net/problem/9372
+ */
 import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        // input
+        int tc = Integer.parseInt(br.readLine());
+
+        // each TestCase
+        for (int t = 0; t < tc; t++) {
+            int answer = 0;
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            // input
+            int[][] plane = new int[M][M];
+
+            for (int i = 0; i < M; i++) { // init
+                for (int j = 0; j < M; j++) {
+                    plane[i][j] = 0;
+                }
+            }
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+
+                plane[x][y] = 1;
+                plane[y][x] = 1;
+            }
+
+            // cal
+            
+        }
+
+
     }
 }
