cbd2f9d980886ffce91068cb1948fb179fd772a4
momo990305@gmail.com <momo990305@gmail.com>
2023-09-18 20:39:07 +0900

    create 20230919_MinCost

---
+++ 2023_09/19_MinCost/src/Main.java
@@ -0,0 +1,9 @@
+/*
+ * Title : 최소비용 구하기
+ * Link : https://www.acmicpc.net/problem/1916
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
